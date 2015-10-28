27284189342ef463fa532e30ac090ba5977c6fed
Parv Jain <parv.wutizup@gmail.com>
2015-10-28 20:58:13 +0530

    cleaning code
    semi-colons are ugly, don't you think?

--- pybook.py
+++ pybook.py
@@ -4,10 +4,10 @@ from subprocess import call
 PyBook is a mini-project implementation of a phonebook in Python 
 '''
 def create():
-    name     = input("Enter name: ");
-    number   = input("Enter phone number: ");
-    email    = input("Enter e-mail address: ");
-    birthday = input("Enter birthday (DD/MM): ");
+    name     = input("Enter name: ")
+    number   = input("Enter phone number: ")
+    email    = input("Enter e-mail address: ")
+    birthday = input("Enter birthday (DD/MM): ")
     file = open('data.pb','a')
     file.write(name+','+number+','+email+','+birthday+'\n')
     file.close()
@@ -28,7 +28,7 @@ def main():
     call(["clear"])
     print("Welcome to PyBook - The Python Phonebook\n")
     print("(C)reate contact\n(D)elete Contact\n(V)iew Contact\n(S)earch Contact\n")
-    choice = input("Enter a valid choice: ");
+    choice = input("Enter a valid choice: ")
     if choice=='C':
         create()
     elif choice=='D':
